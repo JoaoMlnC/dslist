4b9f19156d3860077cf1129e03857ab8c67b46e8
JoaoMlnC <joaomlncampos@hotmail.com>
2023-07-16 18:24:45 -0300

    Aplicando o DTO no controller e service para a api retornar só os dados desejados

--- src/main/java/com/devsuperior/dslist/services/GameService.java
+++ src/main/java/com/devsuperior/dslist/services/GameService.java
@@ -5,7 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-
+import com.devsuperior.dslist.dto.GameMinDTO;
 import com.devsuperior.dslist.entities.Game;
 import com.devsuperior.dslist.repositories.GameRepository;
 
@@ -16,8 +16,9 @@ public class GameService {
     @Autowired
     private GameRepository gameRepository;
     
-    public List<Game> findAll() {
+    public List<GameMinDTO> findAll() {
         List<Game> result = gameRepository.findAll();
-        return result;
+        List<GameMinDTO> dto = result.stream().map(x -> new GameMinDTO(x)).toList();
+        return dto;
     }
 }
