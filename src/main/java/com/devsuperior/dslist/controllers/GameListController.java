9b14fafa52988a726f9844981b7d110cb68c1e7b
JoaoMlnC <joaomlncampos@hotmail.com>
2023-07-16 23:48:55 -0300

    cors config

--- src/main/java/com/devsuperior/dslist/controllers/GameListController.java
+++ src/main/java/com/devsuperior/dslist/controllers/GameListController.java
@@ -3,11 +3,11 @@ package com.devsuperior.dslist.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.repository.query.Param;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+
 import org.springframework.web.bind.annotation.RestController;
 
 import com.devsuperior.dslist.dto.GameListDTO;
